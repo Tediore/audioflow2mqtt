1f0eb72ee21ac8604c66f1e9329fed8376b487f3
Tediore <49540618+Tediore@users.noreply.github.com>
2022-10-21 21:54:16 -0500

    update version number

--- audioflow2mqtt.py
+++ audioflow2mqtt.py
@@ -10,7 +10,7 @@ from threading import Thread as t
 import paho.mqtt.client as mqtt_client
 
 config_file = os.path.exists('config.yaml')
-version = '0.5.0-dev'
+version = '0.5.1-dev'
 
 if config_file:
     with open('config.yaml', 'r') as file:
